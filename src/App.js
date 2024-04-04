3bb61913cc5cf6ef1c31b9073daacdc6c3909a20
MatheusMMarques <matheusdosreis2004@gmail.com>
2024-04-04 17:18:44 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './imagem/bindlogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <nav className="nav">
+          <div className="menu-left">
+            <a href="#section1">Sobre nós</a>
+            <a href="#section2">Visão</a>
+          </div>
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+          <div className="menu-right">
+            <a href="#section3">Entrar</a>
+            <button className="cadastro no-progress">Cadastre-se</button>
+          </div>
+        </nav>
       </header>
+      <section id="section1" className="section">
+        <div className="video-container">
+          {/* Aqui você pode inserir o código para o vídeo */}
+        </div>
+      </section>
     </div>
   );
 }
