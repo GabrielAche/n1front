125f9fd8f55560fee5b52f5176424acf1808b2bc
MatheusMMarques <matheusdosreis2004@gmail.com>
2024-04-04 17:45:20 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 import './App.css';
 import logo from './imagem/bindlogo.png';
 import video from './video/noob.mp4';
@@ -7,21 +6,26 @@ import video from './video/noob.mp4';
 function App() {
   return (
     <div className="App">
+
       <header className="header">
         <nav className="nav">
           <div className="menu-left">
             <a href="#section1">Sobre nós</a>
             <a href="#section2">Visão</a>
           </div>
+
           <div className="logo-container">
             <img src={logo} alt="Logo" className="logo" />
           </div>
+
           <div className="menu-right">
             <a href="#section3">Entrar</a>
             <button className="cadastro no-progress">Cadastre-se</button>
           </div>
+          
         </nav>
       </header>
+
       <section id="section1" className="section">
         <div className="video-container">
           <video className="video" controls>
@@ -30,6 +34,7 @@ function App() {
           </video>
         </div>
       </section>
+
     </div>
   );
 }
