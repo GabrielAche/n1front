cc2635cf79c66587e64371fffed579bd5522578e
Gabriel <gabriel.ache7@gmail.com>
2024-04-04 15:43:54 -0300

    segundo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './imagem/bindlogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
+        <nav>
+          <ul className="menu">
+            <li><a href="#section1">Botão 1</a></li>
+            <li><a href="#section2">Botão 2</a></li>
+            <li><a href="#section3">Botão 3</a></li>
+          </ul>
+        </nav>
       </header>
+      <section id="section1" className="section">
+        <div className="video-container">
+          {/* Aqui você pode inserir o código para o vídeo */}
+        </div>
+      </section>
+      <section id="section2" className="section">
+        {/* Aqui você pode adicionar ícones e emojis decorativos */}
+      </section>
+      <section id="section3" className="section">
+        <div className="signup-form">
+          <h2>Cadastre-se</h2>
+          {/* Aqui você pode adicionar o formulário de cadastro */}
+        </div>
+      </section>
     </div>
   );
 }
